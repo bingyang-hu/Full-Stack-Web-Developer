551435d9b1a89250dfe6388a4c82e45ab5a6ea8b
Zoey Hu <zoey4y@gmail.com>
2020-11-06 13:05:51 -0800

    Create Tuple_Set_Boolean.py

---
+++ Python/Tuple_Set_Boolean.py
@@ -0,0 +1,33 @@
+# Booleans
+
+True
+False
+0
+1
+
+#Tuples
+t=(1,2,3)
+
+print (t[1])
+
+
+
+
+#Set
+
+
+x=set()
+
+x.add(1)
+x.add(2)
+x.add(4)
+x.add(4)
+x.add(0.1)
+
+
+
+# Set is unordered and only takes in unique element.
+print(x)
+
+converted=set([1,1,1,1,1,1,1,2,2,2,2,2,4,4])
+print(converted)

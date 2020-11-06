52849fa64943d916ac1ab5271378c4ff0749320c
Zoey Hu <zoey4y@gmail.com>
2020-11-06 13:05:21 -0800

    Create Strings.py

---
+++ Python/Strings.py
@@ -0,0 +1,19 @@
+mystring="abcdegf"
+
+print(mystring[0])
+
+#Slicing
+
+mystring[2:]
+
+mystring[:3] #d is not included.
+
+
+#Print Formatting
+
+x="Insert another String here:{}".format("INSERTME")
+print(x)
+
+
+y='my name is {name} and I am {age} years old'.format(name='jj',age=35)
+print(y)

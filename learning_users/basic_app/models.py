61929217a446878f48079420915801542c71f634
Zoey Hu <zoey4y@gmail.com>
2020-11-28 14:16:46 -0800

    User model, form and registration
    151,152,153

--- learning_users/basic_app/models.py
+++ learning_users/basic_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+
+
+
+class UserProfileInfo(models.Model):
+
+    user=models.OneToOneField(User,on_delete=models.CASCADE) #Add exta attributes to each User
+
+
+    # additional
+    portfolio_site=models.URLField(blank=True) # Means it's optional
+
+
+
+    profile_pic=models.ImageField(upload_to='profile_pics',blank=True) #Users' picture will be uploaded to media/profile_pics directory
+
+
+    def __str__(self):
+        return self.user.username
--- learning_users/learning_users/settings.py
+++ learning_users/learning_users/settings.py
@@ -89,11 +89,11 @@ DATABASES = {
 
 
 PASSWORD_HASHERS = [
-    'django.contrib.autho.hasher.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
     'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
-    'django.contrib.autho.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
-    'django.contrib.autho.hasher.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
 ]
 
 

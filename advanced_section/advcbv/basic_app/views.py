8fb618f141aaad891019b26beee19d043f368ff0
Zoey Hu <zoey4y@gmail.com>
2020-12-03 21:14:56 -0800

    CBV
    162.

--- advanced_section/advcbv/basic_app/views.py
+++ advanced_section/advcbv/basic_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import View,TemplateView
+from django.views.generic import View,TemplateView,ListView,DetailView
+from . import models  # . means 'look the current directory'
 # from django.http import HttpResponse
 
 # Create your views here.
@@ -8,15 +9,27 @@ from django.views.generic import View,TemplateView
 
 # Class based view CBV
 
+class SchoolListView(ListView):
+    model =  models.School
+
+
+class SchoolDetailView(DetailView):
+    model = models.School
+    template_name= 'basic_app/school_detail.html'
+
+
+
+
+
 # class CBView(View):
 #     def get(self,request):
 #         return HttpResponse('CLASS BASED VIEWS ARE GREAT!')
 
-class IndexView(TemplateView):
-    template_name='index.html'
-
-    def get_context_data(self,**kwargs):
-        context= super().get_context_data(**kwargs)
-
-        context['injectme']='BASIC INJECTION!'
-        return context
+# class IndexView(TemplateView):
+#     template_name='index.html'
+#
+#     def get_context_data(self,**kwargs):
+#         context= super().get_context_data(**kwargs)
+#
+#         context['injectme']='BASIC INJECTION!'
+#         return context

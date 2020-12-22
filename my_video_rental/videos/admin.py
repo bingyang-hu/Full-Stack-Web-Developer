eac03565a14f56c4a7b43930d3c1a192eb4819cd
zoey4y@gmail.com <zoey4y@gmail.com>
2020-12-22 09:23:50 -0800

    197

--- my_video_rental/videos/admin.py
+++ my_video_rental/videos/admin.py
@@ -16,6 +16,6 @@ class MovieAdmin(admin.ModelAdmin): #m aodelname+'admin'
 
     list_display = ['title','release_year','length']
 
-    list_editable = ['length']
+    list_editable = ['length']  
 admin.site.register(models.Customer)
 admin.site.register(models.Movie,MovieAdmin)
